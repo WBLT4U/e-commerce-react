150a372552df4837f2987ed22ffd907bbce6f8a8
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 14:32:52 -0700

    ww

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,62 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import "./search.css";
 
 const Search = ({ CartItem }) => {
-  const [searchTerm, setSearchTerm] = useState("");
- 
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const search = document.querySelector(".search");
-      if (search) {
-        search.classList.toggle("active", window.scrollY > 100);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleSearchKeyPress = (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      // Perform the search action, like navigating to a search results page
-      // For example, you might navigate to a new route with the search term
-      console.log("Search term:", searchTerm);
-      
-    }
-  };
+  // fixed Header
+  window.addEventListener("scroll", function () {
+    const search = document.querySelector(".search");
+    search.classList.toggle("active", window.scrollY > 100);
+  });
 
   return (
     <>
       <section className="search">
         <div className="container c_flex">
-          <div className="logo width">
+          
             <h3>E-Commerce</h3>
-          </div>
+          
 
-          <div className="search-box f_flex">
+          {/* <div className="search-box f_flex">
             <i className="fa fa-search"></i>
-            <input
-              type="text"
-              placeholder="Search and hit enter..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              onKeyPress={handleSearchKeyPress} // Listen for Enter key press
-            />
+            <input type="text" placeholder="Search and hit enter..." />
             <span>All Category</span>
-          </div>
+          </div> */}
 
           <div className="icon f_flex width">
             <i className="fa fa-user icon-circle"></i>
             <div className="cart">
               <Link to="/cart">
                 <i className="fa fa-shopping-bag icon-circle"></i>
-                <span>{CartItem.length > 0 ? CartItem.length : "0"}</span>
+                <span>{CartItem.length === 0 ? "" : CartItem.length}</span>
               </Link>
             </div>
           </div>
