879d99812a101903aaab7512b2ab6f783d70c931
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 13:54:43 -0700

    aa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Header from "./common/header/Header"
-
+import Pages from "./pages/Pages"
 import Data from "./components/Data"
 import Cart from "./common/Cart/Cart"
 import Footer from "./common/footer/Footer"
@@ -59,6 +59,9 @@ function App() {
       <Router>
         <Header CartItem={CartItem} />
         <Switch>
+          <Route path='/' exact>
+            <Pages productItems={productItems} addToCart={addToCart} shopItems={shopItems} />
+          </Route>
           <Route path='/cart' exact>
             <Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
           </Route>
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,9 +1,6 @@
 import React from "react"
 import Home from "../components/MainPage/Home"
-import FlashDeals from "../components/flashDeals/FlashDeals"
-import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
-import Discount from "../components/discount/Discount"
 import Shop from "../components/shops/Shop"
 import Annocument from "../components/annocument/Annocument"
 import Wrapper from "../components/wrapper/Wrapper"
@@ -12,10 +9,7 @@ const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
   return (
     <>
       <Home CartItem={CartItem} />
-      <FlashDeals productItems={productItems} addToCart={addToCart} />
-      <TopCate />
       <NewArrivals />
-      <Discount />
       <Shop shopItems={shopItems} addToCart={addToCart} />
       <Annocument />
       <Wrapper />
