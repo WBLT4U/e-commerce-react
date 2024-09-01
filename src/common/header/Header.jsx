aaf245940d388100a618e3fbc8719a04044d5652
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 14:40:49 -0700

    AA

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "./Header.css"
 import Head from "./Head"
 import Search from "./Search"
-import Navbar from "./Navbar"
+
 
 
 const Header = ({ CartItem }) => {
@@ -10,7 +10,7 @@ const Header = ({ CartItem }) => {
     <>
       <Head />
       <Search CartItem={CartItem} />
-      <Navbar />
+      
       
     </>
   )
