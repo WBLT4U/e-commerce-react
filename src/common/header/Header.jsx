3ef0d44be8c61cddb10d4588e815c0e0d2b1cc1d
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 09:51:50 -0700

    SA

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,8 +1,12 @@
 import React from "react"
 import "./Header.css"
 import Head from "./Head"
+import Home from "./components/Home";
+import AboutUs from "./components/aboutus";
+import ContactUs from "./components/contactus";
 import Search from "./Search"
 import Navbar from "./Navbar"
+import { Routes, Route } from 'react-router-dom';
 
 const Header = ({ CartItem }) => {
   return (
@@ -10,6 +14,12 @@ const Header = ({ CartItem }) => {
       <Head />
       <Search CartItem={CartItem} />
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/aboutus' element={<AboutUs />}></Route>
+        <Route path='/contactus' element={<ContactUs />}></Route>
+        
+      </Routes>
     </>
   )
 }
