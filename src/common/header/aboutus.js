47045dba8ff13d33539517a89a5f38a3334201d6
Taofiq Abdulsalam <taofiqabdulsalam48@gmail.com>
2024-09-01 09:27:26 -0700

    head

--- src/common/header/aboutus.js
+++ src/common/header/aboutus.js
@@ -1,4 +1,4 @@
-
+import React from "react"
 //Create a homepage component...
 const AboutUs = () => {
   return (
--- src/common/header/contactus.js
+++ src/common/header/contactus.js
@@ -1,3 +1,4 @@
+import React from "react"
 //Create a homepage component...
 const ContactUs = () => {
   return (
